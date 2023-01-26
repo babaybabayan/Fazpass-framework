bba0002742cd820b7d7674c79207db8ee34fd03e
Akbar Putera <akbarputera65@gmail.com>
2023-01-26 12:31:36 +0700

    version 1.0,2

---
+++ Sources/Fazpass_build.xcframework/ios-arm64_x86_64-simulator/Fazpass_build.framework/Headers/Fazpass_build.h
@@ -0,0 +1,18 @@
+//
+//  Fazpass_build.h
+//  Fazpass_build
+//
+//  Created by Binar - Mei on 26/01/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Fazpass_build.
+FOUNDATION_EXPORT double Fazpass_buildVersionNumber;
+
+//! Project version string for Fazpass_build.
+FOUNDATION_EXPORT const unsigned char Fazpass_buildVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Fazpass_build/PublicHeader.h>
+
+
